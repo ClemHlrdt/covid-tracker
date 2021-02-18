13547c817b2fb1ca25108436f22a805101042f27
Clément Hélardot <clement.helardot@gmail.com>
2021-02-18 18:03:37 +0100

    Added ScrollTop components

---
+++ src/components/UI/ScrollTop.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function ScrollTop() {
+  const onClickHandler = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    <div onClick={onClickHandler}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        className="fixed h-12 text-gray-200 bottom-5 right-5"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z"
+        />
+      </svg>
+    </div>
+  );
+}
