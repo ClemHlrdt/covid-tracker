6c1df4f8fc2db00cafc1d87c1e5ddd9c6f9a77fa
Clément Hélardot <clement.helardot@gmail.com>
2021-02-23 17:17:25 +0100

    Fix padding for mobile viewports

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,11 +3,9 @@ import svg from "./undraw_not-found.svg";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center h-full">
-      <h3 className="mt-8 mb-8 text-3xl font-bold text-gray-800">
-        Page not found!
-      </h3>
-      <img src={svg} alt="page not found" className="max-w-md p-10" />
+    <div className="relative flex flex-col items-center justify-center h-full bg-gray-800">
+      <img src={svg} alt="page not found" className="md:max-w-md" />
+      <h3 className="p-4 text-3xl font-bold text-white">Page not found!</h3>
     </div>
   );
 }
--- src/components/UI/NoData/NoData.js
+++ src/components/UI/NoData/NoData.js
@@ -4,8 +4,10 @@ import svg from "./undraw_empty.svg";
 export default function NoData() {
   return (
     <div className="px-16 pt-32 pb-16 text-center bg-white rounded-lg">
-      <img src={svg} alt="No data logo" className="max-w-md" />
-      <h3 className="mt-8 text-3xl font-bold text-gray-800 ">No data found!</h3>
+      <img src={svg} alt="No data logo" className="md:max-w-sm" />
+      <h3 className="mt-8 font-bold text-gray-800 md:text-3xl ">
+        No data found!
+      </h3>
     </div>
   );
 }
