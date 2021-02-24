432cf64931d9ff8761b6a09204d2339ffd42ae65
Clément Hélardot <clement.helardot@gmail.com>
2021-02-24 15:17:17 +0100

    Changed map provider

--- src/containers/Map/Map.js
+++ src/containers/Map/Map.js
@@ -18,8 +18,8 @@ export default function Map() {
         style={{ height: "100%" }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <LocationMarker />
       </MapContainer>
