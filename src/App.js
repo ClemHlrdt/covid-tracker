72aa2ef544132b3bd4be7a8d965aaae349d6c71d
Clément Hélardot <clement.helardot@gmail.com>
2021-02-25 20:07:00 +0100

    Removed unused dependency, changed image format to webp and added lazy loading for components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
-import Countries from "./containers/Countries/Countries";
-import Global from "./containers/Global/Global";
+
 import Navbar from "./components/Navbar/Navbar";
-import NotFound from "./components/NotFound/NotFound";
-import Map from "./containers/Map/Map";
+
+import Spinner from "./components/UI/Spinner/Spinner";
+
+const Global = React.lazy(() => import("./containers/Global/Global"));
+const Countries = React.lazy(() => import("./containers/Countries/Countries"));
+const Map = React.lazy(() => import("./containers/Map/Map"));
+const NotFound = React.lazy(() => import("./components/NotFound/NotFound"));
 
 function App() {
   return (
     <div className="relative h-screen">
-      <Router>
-        <Navbar />
-        <main className="h-full mx-auto">
-          <Switch>
-            <Route path="/" exact>
-              <Global />
-            </Route>
-            <Route path="/countries">
-              <Countries />
-            </Route>
-            <Route path="*">
-              <Map />
-            </Route>
-            <Route path="*">
-              <NotFound />
-            </Route>
-          </Switch>
-        </main>
-      </Router>
-      <ToastContainer autoClose={5000} closeOnClick position="bottom-right" />
+      <Suspense fallback={<Spinner />}>
+        <Router>
+          <Navbar />
+          <main className="h-full mx-auto">
+            <Switch>
+              <Route path="/" exact>
+                <Global />
+              </Route>
+              <Route path="/countries">
+                <Countries />
+              </Route>
+              <Route path="*">
+                <Map />
+              </Route>
+              <Route path="*">
+                <NotFound />
+              </Route>
+            </Switch>
+          </main>
+        </Router>
+      </Suspense>
     </div>
   );
 }
